17c523c4d9729759dda1871a9b1792127085d570
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-11-24 22:46:42 +0100

    Add exercise 4 module 2

---
+++ modulo-2-leccion-01-ejercicio-4/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const title = document.querySelector ('.js_title');
+
+const nombre = document.querySelector ('.js_nombre');
+
+const adalaber = title.innerHTML + ' ' + nombre.innerHTML;
+
+title.innerHTML = adalaber;
