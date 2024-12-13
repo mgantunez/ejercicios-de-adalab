514fb47910b5286afa7e5e5a25df2a2c27be32b7
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-13 23:22:39 +0100

    Add tasks js

--- modulo-2-ADATASKS/js/main.js
+++ modulo-2-ADATASKS/js/main.js
@@ -5,8 +5,34 @@ const tasks = [
     { name: "Comprar pilas", completed: true, id: 2 },
     { name: "Poner una lavadora de blancos", completed: true, id: 3 },
     {
-      name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-      completed: false,
-      id: 4,
+        name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+        completed: false,
+        id: 4,
     },
-  ];
+];
+
+const tasksUl = document.querySelector('.js_tasksUl');
+
+// Pintar las tareas del array en el listado
+
+tasks.forEach((oneTask) => {
+    tasksUl.innerHTML += `
+      <li class="task ${oneTask.completed ? 'completed' : ''}">
+        <input type="checkbox" ${oneTask.completed ? 'checked' : ''}>
+        <p>${oneTask.name}</p>
+      </li>
+    `;
+  });
+        
+
+
+
+ 
+
+
+
+
+
+
+
+
